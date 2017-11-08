fc5322cfa5f72a87c30e5cfac89a6a4d94b5dbbf
Jeff <jefferson556@gmail.com>
2017-11-08 13:38:53 -0300

    asdasdasd

--- educanding/app/controllers/application_controller.rb
+++ educanding/app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def after_sign_in_path_for(resource_or_scope)
+    session[:cuenta]=current_user.account_type
+  end
 end
