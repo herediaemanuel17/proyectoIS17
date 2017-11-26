5a6bfae62efa8c93538cb7285eda1274f682a9ce
Hernan Espinola <espinolahernan1993@gmail.com>
2017-11-26 18:31:41 -0300

    preguntas con menos respuestas y listado de preguntas del usuario logueado en el momento

--- educanding/app/controllers/educanding_controller.rb
+++ educanding/app/controllers/educanding_controller.rb
@@ -4,11 +4,6 @@ class EducandingController < ApplicationController
       @question=Question.all.order("created_at desc").limit(25)
     if user_signed_in?
       @questionMiUni= Question.where(Univercity_id:current_user.Univercity_id)
-      questions=Question.all
-      questions.each do |q|
-        cantidad=Answer.where(Question_id:q.id).count  
-      end
-      @questionMenosRespondidas=Question.all
     end
   end
 
