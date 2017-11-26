1acad0ac0d3fa404a401c143306eb76164e73921
herediaemanuel17 <herediaemanuel17@gmail.com>
2017-11-26 20:35:46 -0300

    un archivo mas para configurar heroku

--- educanding/db/schema.rb
+++ educanding/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171109164738) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string "descripcion"
     t.integer "User_id"
